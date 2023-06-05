52b13945bcde1c4d8a77fcb025fb98a50dac997e
herrgromov <herrgromov@MacBook-Pro-von-herrgromov.local>
2023-06-05 20:17:50 +0200

    new lesson about syntaxis from book - 3 i am finisch

--- lessen-1.js
+++ lessen-1.js
@@ -1,4 +1,4 @@
-//Syntaxis
+//Syntaxis & Mistakes
 'Uncaught SyntaxError:' - ненайденая синтаксическая ошибка.
 'Unexpected Token' ; - инторпритатор прочел что-то, чего прочесть не ожидал, а иненно: точку с запятой - ; .
 () - скобки всегда ходят парами.
@@ -14,3 +14,20 @@ let одиночныйПробел = " "; //undefined
 let полноеПриветствие = перваяЧастьПриветствия + одиночныйПробел +
 втораяЧастьПриветствия; //undefined
 полноеПриветствие; //'Привет, Мир'
+
+
+// Example 2
+let komboA = 6.75; //undefined
+let komboB = 3.15; //undefined
+let komboC = 9.95; //undefined
+let komboD = 12.95; //undefined
+let stoimNabora = komboA + komboB + komboC + komboD; //undefined
+stoimNabora; //32.8
+let vsegoNaborov = 4; //undefined
+let srednqqStoimoctNabora = stoimNabora / vsegoNaborov; //undefined
+srednqqStoimoctNabora; //8.2
+
+//new terms + new Kombo
+vsegoNaborov = 5;
+srednqqStoimoctNabora = stoimNabora + komboNew; //50.78999999999999
+srednqqStoimoctNabora = stoimNabora / vsegoNaborov; //6.56
